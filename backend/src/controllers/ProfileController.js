45f9c1e9b5daf8d999d0bf334412ae2e9a280d93
Edson Chicale <edsondejesusduque@gmail.com>
2020-11-26 10:36:06 +0100

    New updates

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -5,7 +5,7 @@ module.exports = {
         const ong_id = request.headers.authorization;
 
         const incidents = await connection('incidents')
-            .where('ong_id', ong_id)
+            .where('ong_id', ong_id) 
             .select('*');
 
         return response.json(incidents);
--- front/src/pages/Profile/index.js
+++ front/src/pages/Profile/index.js
@@ -62,7 +62,7 @@ export default function Profile() {
                             <p>{incident.description}</p>
 
                             <strong>VALOR</strong>
-                            <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+                            <p>{Intl.NumberFormat('pt-AO', { style: 'currency', currency: 'AOA' }).format(incident.value)}</p>
                             <button type="button" onClick={() => handleDeleteIncident(incident.id)}>
                                 <FiTrash2 size={20} color="#a8a8b3" />
                             </button>
